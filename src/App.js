6f511d02cf6e34c9115dd4c18cdca139f6c33d12
Jordan Leslie <this.codedev@gmail.com>
2018-02-20 08:36:52 -0600

    Use create-react-app to generate file structure, node is running on port 8080

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>Hello, Bears Team 1!</div>;
+  }
+}
+
+export default App;
