0f7359fa02dfb28091817157713130392af10d1d
Jordan Leslie <this.codedev@gmail.com>
2018-02-20 21:47:49 -0600

    app.listen() now includes error handling.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+
+app.set("port", process.env.PORT || 3001);
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/ping", function(req, res) {
@@ -12,4 +14,10 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3001, (err) => {
+  if(err) {
+    console.log("error", err);
+  } else {
+    console.log(`Server.js is running on port ${app.get("port")}`)
+  }
+});
