7693df5d6a417f6d7791ad7c13d4597edf47ec0b
Jordan Johnson <jordanleo7@hotmail.com>
2018-02-27 08:37:21 -0600

    Testing Prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import Main from "./Main";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
-    return <div>Hello, Bears Team 1!</div>;
+    return (
+      <div>
+        <Nav />
+        <Main />
+        <Footer />
+      </div>
+    );
   }
 }
 
