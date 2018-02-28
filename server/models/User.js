17f2264dbd53a1414bcd0df1dd6814e59da27c30
Jordan Leslie <this.codedev@gmail.com>
2018-02-28 15:11:38 -0600

    Expand User schema to inlude schema types

--- server/models/User.js
+++ server/models/User.js
@@ -3,16 +3,31 @@ const Schema = mongoose.Schema;
 // Define User Schema
 const UserSchema = new Schema({
     userInfo: {
-        username: String,
-        firstName: String,
-        lastName: String, 
-        gitId: Number
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        }, 
+        gitId: {
+            type: Number
+        }
     },
     stats: {
         tweets: [Number],
-        likes: [],
-        followers: [],
-        following: []
+        likes: [Number],
+        followers: [Number],
+        following: [Number]
     },
     avatarUrl: ""
 });
