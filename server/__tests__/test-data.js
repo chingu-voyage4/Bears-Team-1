0ab098f1ba267399ade8179dc6f71f50b180f830
Jordan Leslie <this.codedev@gmail.com>
2018-03-04 11:00:52 -0600

    Add testUsers User object

---
+++ server/__tests__/test-data.js
@@ -0,0 +1,24 @@
+let testUser = {
+  userInfo: {
+    username: "misoawesome",
+    firstName: "Miso",
+    lastName: "Awesomely"
+  }
+};
+
+let testUsers = [
+  {
+    userInfo: {
+      username: "misoawesome",
+      firstName: "Miso",
+      lastName: "Awesomely"
+    },
+    userInfo: {
+      username: "looplenny",
+      firstName: "Lenny",
+      lastName: "Looper"
+    }
+  }
+];
+
+module.exports = { testUser, testUsers };
