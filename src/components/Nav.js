0e06951728dae68be01d4079ab27b7a247af479e
Jordan Johnson <jordanleo7@hotmail.com>
2018-03-03 22:55:57 -0600

    Added navbar layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,26 @@ import React, { Component } from "react";
 
 class Nav extends Component {
   render() {
-    return <div>Nav</div>;
+    return (
+      <div className="container">
+        <nav className="navbar">
+          <ul className="navbar--left">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>Notifications</li>
+          </ul>
+          <ul className="navbar--icon">
+            <li>Icon</li>
+          </ul>
+          <ul className="navbar--right">
+            <li>Search</li>
+            <li>Pic</li>
+            <li>Tweety</li>
+          </ul>
+        </nav>
+      </div>
+    );
   }
 }
 
