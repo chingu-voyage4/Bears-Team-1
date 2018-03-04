6c2bc4fcda1ef328ee60544b8a8a273bc7107a40
Jordan Johnson <jordanleo7@hotmail.com>
2018-03-04 11:33:09 -0600

    Added Tweet feed layout

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,7 +2,99 @@ import React, { Component } from "react";
 
 class Feed extends Component {
   render() {
-    return <div className="container">Feed</div>;
+    return (
+      <div className="container">
+        <ol className="feed--list">
+          <li className="feed--list-item">
+            <div>
+              <div className="feed--avatar" />
+            </div>
+            <div>
+              <span className="">Real Name</span>
+              <span className="">@username</span>
+              <span className=""> ~ 1h</span>
+            </div>
+            <div>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+            <div className="feed--list-item--actions">
+              <span>Reply</span>
+              <span>Retweet</span>
+              <span>Like</span>
+            </div>
+          </li>
+
+          <li className="feed--list-item">
+            <div>
+              <div className="feed--avatar" />
+            </div>
+            <div>
+              <span className="">Real Name</span>
+              <span className="">@username</span>
+              <span className=""> ~ 1h</span>
+            </div>
+            <div>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+            <div className="feed--list-item--actions">
+              <span>Reply</span>
+              <span>Retweet</span>
+              <span>Like</span>
+            </div>
+          </li>
+
+          <li className="feed--list-item">
+            <div>
+              <div className="feed--avatar" />
+            </div>
+            <div>
+              <span className="">Real Name</span>
+              <span className="">@username</span>
+              <span className=""> ~ 1h</span>
+            </div>
+            <div>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+            <div className="feed--list-item--actions">
+              <span>Reply</span>
+              <span>Retweet</span>
+              <span>Like</span>
+            </div>
+          </li>
+
+          <li className="feed--list-item">
+            <div>
+              <div className="feed--avatar" />
+            </div>
+            <div>
+              <span className="">Real Name</span>
+              <span className="">@username</span>
+              <span className=""> ~ 1h</span>
+            </div>
+            <div>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+            <div className="feed--list-item--actions">
+              <span>Reply</span>
+              <span>Retweet</span>
+              <span>Like</span>
+            </div>
+          </li>
+        </ol>
+      </div>
+    );
   }
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <div>Footer</div>;
+    return <div>2018 Bears-Team-1 GitHub About</div>;
   }
 }
 
