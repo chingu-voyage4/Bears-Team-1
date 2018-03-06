d07020d4642defd4090b40d12df8997abd01ed64
Jordan Johnson <jordanleo7@hotmail.com>
2018-03-05 18:11:46 -0600

    Mobile first feed layout

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import reply from "../assets/iconmonstr-speech-bubble-15-240.png";
+import share from "../assets/iconmonstr-share-1-240.png";
+import like from "../assets/iconmonstr-favorite-3-240.png";
 
 class Feed extends Component {
   render() {
@@ -10,42 +13,26 @@ class Feed extends Component {
               <div className="feed--avatar" />
             </div>
             <div>
-              <span className="">Real Name</span>
-              <span className="">@username</span>
-              <span className=""> ~ 1h</span>
-            </div>
-            <div>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </span>
-            </div>
-            <div className="feed--list-item--actions">
-              <span>Reply</span>
-              <span>Retweet</span>
-              <span>Like</span>
-            </div>
-          </li>
-
-          <li className="feed--list-item">
-            <div>
-              <div className="feed--avatar" />
-            </div>
-            <div>
-              <span className="">Real Name</span>
-              <span className="">@username</span>
-              <span className=""> ~ 1h</span>
-            </div>
-            <div>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </span>
-            </div>
-            <div className="feed--list-item--actions">
-              <span>Reply</span>
-              <span>Retweet</span>
-              <span>Like</span>
+              <div className="feed--user">
+                <span className="feed--username">@username</span>
+                <span className="feed--date">{" - "}1h ago</span>
+              </div>
+              <div>
+                <span className="feed--message">
+                  I live my life one quarter mile at a time.
+                </span>
+              </div>
+              <ul className="feed--list-item--actions">
+                <li>
+                  <img src={reply} alt="reply" />3
+                </li>
+                <li>
+                  <img src={share} alt="share" />9
+                </li>
+                <li>
+                  <img src={like} alt="like" />20
+                </li>
+              </ul>
             </div>
           </li>
 
@@ -54,20 +41,26 @@ class Feed extends Component {
               <div className="feed--avatar" />
             </div>
             <div>
-              <span className="">Real Name</span>
-              <span className="">@username</span>
-              <span className=""> ~ 1h</span>
-            </div>
-            <div>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </span>
-            </div>
-            <div className="feed--list-item--actions">
-              <span>Reply</span>
-              <span>Retweet</span>
-              <span>Like</span>
+              <div className="feed--user">
+                <span className="feed--username">@username</span>
+                <span className="feed--date">{" - "}Mar 3, 2018</span>
+              </div>
+              <div>
+                <span className="feed--message">
+                  Que mas carne ni mas carne, asi viene el sandwich, man!
+                </span>
+              </div>
+              <ul className="feed--list-item--actions">
+                <li>
+                  <img src={reply} alt="reply" />1000
+                </li>
+                <li>
+                  <img src={share} alt="share" />2000
+                </li>
+                <li>
+                  <img src={like} alt="like" />9000
+                </li>
+              </ul>
             </div>
           </li>
 
@@ -76,20 +69,81 @@ class Feed extends Component {
               <div className="feed--avatar" />
             </div>
             <div>
-              <span className="">Real Name</span>
-              <span className="">@username</span>
-              <span className=""> ~ 1h</span>
+              <div className="feed--user">
+                <span className="feed--username">@username</span>
+                <span className="feed--date">{" - "}Mar 3, 2018</span>
+              </div>
+              <div>
+                <span className="feed--message">Hello World!</span>
+              </div>
+              <ul className="feed--list-item--actions">
+                <li>
+                  <img src={reply} alt="reply" />9
+                </li>
+                <li>
+                  <img src={share} alt="share" />20
+                </li>
+                <li>
+                  <img src={like} alt="like" />3
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <li className="feed--list-item">
+            <div>
+              <div className="feed--avatar" />
             </div>
             <div>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </span>
+              <div className="feed--user">
+                <span className="feed--username">@username</span>
+                <span className="feed--date">{" - "}Mar 3, 2018</span>
+              </div>
+              <div>
+                <span className="feed--message">
+                  It's not who I am underneath, but what I do that defines me.
+                </span>
+              </div>
+              <ul className="feed--list-item--actions">
+                <li>
+                  <img src={reply} alt="reply" />
+                  <span>91</span>
+                </li>
+                <li>
+                  <img src={share} alt="share" />
+                  <span>50</span>
+                </li>
+                <li>
+                  <img src={like} alt="like" />
+                  <span>800</span>
+                </li>
+              </ul>
             </div>
-            <div className="feed--list-item--actions">
-              <span>Reply</span>
-              <span>Retweet</span>
-              <span>Like</span>
+          </li>
+
+          <li className="feed--list-item">
+            <div>
+              <div className="feed--avatar" />
+            </div>
+            <div>
+              <div className="feed--user">
+                <span className="feed--username">@username</span>
+                <span className="feed--date">{" - "}Mar 3, 2018</span>
+              </div>
+              <div>
+                <span className="feed--message">First post</span>
+              </div>
+              <ul className="feed--list-item--actions">
+                <li>
+                  <img src={reply} alt="reply" />1
+                </li>
+                <li>
+                  <img src={share} alt="share" />0
+                </li>
+                <li>
+                  <img src={like} alt="like" />2
+                </li>
+              </ul>
             </div>
           </li>
         </ol>
