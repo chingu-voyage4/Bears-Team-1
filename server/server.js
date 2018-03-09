72e17ee84f163bb1886fad6283c115fcb6363ab1
Jordan Leslie <this.codedev@gmail.com>
2018-03-09 07:54:35 -0600

    Extract mongoose configuration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/v4Bears01";
 const path = require("path");
+
+// Connect to database
+mongoose
+  .connect(MONGO_URI)
+  .then(res => {
+    console.log(`Connected to ${MONGO_URI}`);
+  })
+  .catch(err => {
+    if (err) console.log("err", err);
+  });
+// Use native promises
+mongoose.Promise = global.Promise;
 
 const { app } = require("./app");
 const PORT = process.env.PORT || 3001;
