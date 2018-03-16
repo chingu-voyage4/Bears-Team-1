87ea8e0b2b9a6c2c2758b399b7d981be080930a0
Jordan Johnson <jordanleo7@hotmail.com>
2018-03-16 12:59:53 -0500

    Added client delete scoop request

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,6 +4,7 @@ import moment from "moment";
 import reply from "../assets/iconmonstr-speech-bubble-2-240.png";
 import share from "../assets/iconmonstr-retweet-1-240.png";
 import like from "../assets/iconmonstr-favorite-2-240.png";
+import liked from "../assets/iconmonstr-favorite-1-240.png";
 
 class Feed extends Component {
   constructor(props) {
@@ -14,6 +15,15 @@ class Feed extends Component {
     this.handleLike = this.handleLike.bind(this);
   }
 
+  likeButton(props) {
+    const isLiked = props.isLiked;
+    if (!isLiked) {
+      return <img src={like} alt="like" onClick={this.handleLike} />;
+    } else {
+      return <img src={liked} alt="liked" onClick={this.handleLike} />;
+    }
+  }
+
   handleLike(event) {
     event.preventDefault();
     axios
@@ -21,6 +31,19 @@ class Feed extends Component {
         tweet_id: "5aa05812fcbbc803417de0b6",
         action: "like"
       })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleDelete(event) {
+    event.preventDefault();
+    console.log("delete", event.target.dataset.scoopid);
+    axios
+      .delete("/tweet/" + event.target.dataset.scoopid)
       .then(response => {
         console.log(response);
       })
@@ -74,7 +97,15 @@ class Feed extends Component {
                     </li>
                     <li>
                       <form onSubmit={this.handleLike}>
-                        <img src={like} alt="like" onClick={this.handleLike} />0
+                        {this.likeButton("isLiked")}0
+                      </form>
+                    </li>
+                    <li>
+                      <form
+                        data-scoopid={scoop._id}
+                        onSubmit={this.handleDelete}
+                      >
+                        <input type="submit" value="Delete" />
                       </form>
                     </li>
                   </ul>
