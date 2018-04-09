1d1d6dfe1058ee7205e222a7797583f09cf9c96f
Jordan Johnson <jordanleo7@hotmail.com>
2018-04-08 19:23:59 -0500

    Added material design styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -36,20 +36,28 @@ class SearchBar extends Component {
     return (
       <div>
         <div className="search--container">
-          <form onSubmit={this.handleSubmit} className="">
-            <input
-              className="search--input"
-              type="text"
-              value={this.state.searchText}
-              onChange={this.handleChange}
-              placeholder="Search Scoop..."
-            />
-            <img src={search} alt="search" className="search--icon" />
+          <div className="search">
+            <h1 className="search--header">Who's on your mind?</h1>
+            <form onSubmit={this.handleSubmit} className="search--form">
+              <input
+                className="search--input"
+                type="text"
+                value={this.state.searchText}
+                onChange={this.handleChange}
+                placeholder="Search by username"
+              />
+              {
+                //<img src={search} alt="search" className="search--icon" />
+              }
 
-            <input type="submit" value="Submit" className="" />
-          </form>
+              <input
+                type="submit"
+                value="Search"
+                className="search--form--submit-button"
+              />
+            </form>
+          </div>
         </div>
-
         <div>
           {searchResults === "" ? <p>...</p> : JSON.stringify(searchResults)}
         </div>
